4cbd6a61406841b7fdc63cbf7f30bdc9cee2d3d7
Evan Feng <evanfeng55@gmail.com>
2016-11-15 20:27:15 -0500

    read input

---
+++ src/com/yf833/Biography.java
@@ -0,0 +1,18 @@
+package com.yf833;
+
+
+public class Biography {
+
+    public String person;
+    public String field;
+    public String description;
+
+    public Biography(String person, String field, String description){
+        this.person = person;
+        this.field = field;
+        this.description = description;
+    }
+
+
+
+}
--- src/com/yf833/Main.java
+++ src/com/yf833/Main.java
@@ -1,8 +1,81 @@
 package com.yf833;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static int num_entries;
+    public static final String STOPWORDS_FILE = "./stopwords.txt";
+    public static HashSet<String> stopwords;
+    public static ArrayList<Biography> training_bios;
+    public static ArrayList<Biography> test_bios;
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        getInput(args[0], args[1]);
+
+        System.out.println();
+
+
     }
+
+
+
+    // read stopwords, training corpus entries, and test corpus entries
+    public static void getInput(String corpuspath, String N) throws FileNotFoundException {
+
+        //read in the number of entries in the corpus
+        num_entries = Integer.parseInt(N);
+
+        //read in the set of stopwords
+        stopwords = new HashSet<>();
+        Scanner stopword_scan = new Scanner(new File(STOPWORDS_FILE), "UTF-8");
+        String stopwords_str = stopword_scan.useDelimiter("\\A").next();
+        stopword_scan.close();
+
+        for(String s : stopwords_str.split("\\s+")){
+            stopwords.add(s);
+        }
+
+        //read in the first N entries as training data
+        Scanner corpus_scan = new Scanner(new File(corpuspath), "UTF-8");
+        training_bios = new ArrayList<>();
+        test_bios = new ArrayList<>();
+
+        for(int i=0; i<num_entries; i++){
+            String person = corpus_scan.nextLine();
+            String field = corpus_scan.nextLine();
+            String description = corpus_scan.nextLine();
+
+            training_bios.add(new Biography(person, field, description));
+            //skip empty line
+            if(corpus_scan.hasNextLine()){
+                corpus_scan.nextLine();
+            }
+        }
+
+        //read in the next entries into test data
+        while(corpus_scan.hasNextLine()){
+            String person = corpus_scan.nextLine();
+            String field = corpus_scan.nextLine();
+            String description = corpus_scan.nextLine();
+
+            test_bios.add(new Biography(person, field, description));
+
+            //skip empty line
+            if(corpus_scan.hasNextLine()){
+                corpus_scan.nextLine();
+            }
+        }
+
+
+    }
+
+
+
 }
